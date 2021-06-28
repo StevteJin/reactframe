457d024c0ab4dd23b380e292e13b0a11085a34a7
jinqingsong <1211050148@qq.com>
2021-06-28 13:43:23 +0800

    设备状态

--- src/components/jhomelt/index.jsx
+++ src/components/jhomelt/index.jsx
@@ -61,67 +61,67 @@ function Jhomelt() {
 
   return (
     <div className="big_e_box">
-        <div className="e_top_title">
-          <img src={require("../../images/Jhome/icon-1.png")} alt="" />
-          <span>设备状态</span>
-        </div>
-        <div>
-          {equipmentState.map((item, index) => {
-            return (
-              <div key={index} className="e_box">
-                <div className="e_name">{item.name}</div>
-                <div className="e_state e_state1">
-                  <span>{item.state1}</span>
-                  <span>{item.stateNum1}</span>个
-                </div>
-                <div className="e_state e_state2">
-                  <span>{item.state2}</span>
-                  <span>{item.stateNum2}</span>个
-                </div>
+      <div className="e_top_title">
+        <img src={require("../../images/Jhome/icon-1.png")} alt="" />
+        <span>设备状态</span>
+      </div>
+      <div>
+        {equipmentState.map((item, index) => {
+          return (
+            <div key={index} className="e_box">
+              <div className="e_name">{item.name}</div>
+              <div className="e_state e_state1">
+                <span>{item.state1}</span>
+                <span>{item.stateNum1}</span>个
               </div>
-            );
-          })}
-        </div>
-        <div className="e_top_title e_top_title2">
-          <img src={require("../../images/Jhome/icon-1.png")} alt="" />
-          <span>环境监控</span>
-        </div>
-        <div className="pie_box">
-          <div className="pie_left">
-            <div className="pie_left_top">
-              <div className="pie_zhi">污染指数</div>
-              <div className="pie_num">121</div>
+              <div className="e_state e_state2">
+                <span>{item.state2}</span>
+                <span>{item.stateNum2}</span> {item.stateNum2 ? "个" : ""}
+              </div>
             </div>
-            <div className="pie_left_bottom">轻度污染</div>
+          );
+        })}
+      </div>
+      <div className="e_top_title e_top_title2">
+        <img src={require("../../images/Jhome/icon-1.png")} alt="" />
+        <span>环境监控</span>
+      </div>
+      <div className="pie_box">
+        <div className="pie_left">
+          <div className="pie_left_top">
+            <div className="pie_zhi">污染指数</div>
+            <div className="pie_num">121</div>
           </div>
-          <div className="pie_right">
-            {jiankong.map((item, index) => {
-              return (
-                <div key={index} className="pie_right_co">
-                  <span className="pie_right_name">{item.name}</span>
-                  <span className="pie_right_note">{item.note}</span>
-                  <span className="pie_right_value">{item.value}</span>
-                </div>
-              );
-            })}
-          </div>
+          <div className="pie_left_bottom">轻度污染</div>
         </div>
-        <div className="e_top_title e_top_title3">
-          <img src={require("../../images/Jhome/icon-1.png")} alt="" />
-          <span>视频监控</span>
-        </div>
-        <div className="video_box"></div>
-        <div className="video_btn_box">
-          {videoData.map((item, index) => {
+        <div className="pie_right">
+          {jiankong.map((item, index) => {
             return (
-              <div className="video_btn" key={index}>
-                <img src={require("../../images/Jhome/dian.png")} alt="" />
-                &nbsp;&nbsp;
-                {item.name}
+              <div key={index} className="pie_right_co">
+                <span className="pie_right_name">{item.name}</span>
+                <span className="pie_right_note">{item.note}</span>
+                <span className="pie_right_value">{item.value}</span>
               </div>
             );
           })}
         </div>
+      </div>
+      <div className="e_top_title e_top_title3">
+        <img src={require("../../images/Jhome/icon-1.png")} alt="" />
+        <span>视频监控</span>
+      </div>
+      <div className="video_box"></div>
+      <div className="video_btn_box">
+        {videoData.map((item, index) => {
+          return (
+            <div className="video_btn" key={index}>
+              <img src={require("../../images/Jhome/dian.png")} alt="" />
+              &nbsp;&nbsp;
+              {item.name}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
